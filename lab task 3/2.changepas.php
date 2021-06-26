5efd445b8e0f0558ad17b5d34a0b3549b5f992bb
OisheeAnjum <76399587+OisheeAnjum@users.noreply.github.com>
2021-06-27 04:07:31 +0600

    Add files via upload

---
+++ lab task 3/2.changepas.php
@@ -0,0 +1,48 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Register Form</title>
+</head>
+<body>
+	<form method="post" action="">
+  <table>
+   <tr>
+    <td>Current Pasword :</td>
+    <td><input type="Pasword" name="cpas"></td>
+   </tr>
+   
+   
+   <tr>
+    <td>New Pasword :</td>
+    <td><input type="Pasword" name="npas"></td>
+   </tr>
+   <tr>
+    <td>Retype New Pasword :</td>
+    <td><input type="Pasword" name="rnpas"></td>
+   </tr>
+ </td>
+   </tr>
+
+
+
+
+
+   <tr>
+    <td></td>
+    <td><input type="submit" name="submit"></td>
+   </tr>
+ 
+  
+  </table>
+ </form>
+
+
+</body>
+</html>
+<?php
+
+    
+
+
+?>
